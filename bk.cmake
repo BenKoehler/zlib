5129879fe5a7a21490e523c0dcd28003ad57c83f
BenKoehler <ben.koehler@web.de>
2022-10-16 10:49:15 +0200

    replace CMAKE_CURRENT_SOURCE_DIR with PROJECT_SOURCE_DIR

--- bk.cmake
+++ bk.cmake
@@ -1,4 +1,4 @@
 function(link_zlib target MOD)
-    target_link_libraries(${target} ${MOD} ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/zlib/lib/libz.so)
-    target_include_directories(${target} SYSTEM ${MOD} ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/zlib/include)
+    target_link_libraries(${target} ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/zlib/lib/libz.so)
+    target_include_directories(${target} SYSTEM ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/zlib/include)
 endfunction()
