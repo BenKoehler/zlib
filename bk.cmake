c8eb7203c2d2a40c613ea3d3ce28f83586d92dd1
BenKoehler <ben.koehler@web.de>
2022-10-25 22:03:39 +0200

    add version macro

--- bk.cmake
+++ bk.cmake
@@ -1,3 +1,4 @@
+set(BK_ZLIB_VERSION "1.2.12")
 function(link_zlib target MOD)
     target_link_libraries(${target} ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/zlib/lib/libz.so)
     target_include_directories(${target} SYSTEM ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/zlib/include)
